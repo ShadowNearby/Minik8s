a18834597d1a4208f21bafff5ae7c6206937ece7
yan <yan.jingsheng@outlook.com>
2024-04-26 15:23:39 +0000

    update pkgs/apiserver/handler/nodehandler.go.
    Signed-off-by: yan <yan.jingsheng@outlook.com>

--- pkgs/apiserver/handler/nodehandler.go
+++ pkgs/apiserver/handler/nodehandler.go
@@ -14,15 +14,15 @@ func CreateNodeHandler(c *gin.Context) {
 	var nodeConfig core.Node
 	err := c.Bind(&nodeConfig)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, httpData("needs node config type"))
+		c.JSON(http.StatusBadRequest, gin.H{"error": "needs node config type"})
 		return
 	}
 	nodeName := fmt.Sprintf("/nodes/object/%s", nodeConfig.NodeMetaData.Name)
 	err = storage.Put(nodeName, nodeConfig)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, httpData("cannot store data"))
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "cannot store data"})
 	}
-	c.JSON(http.StatusOK, "")
+	c.JSON(http.StatusOK, gin.H{})
 }
 
 // GetNodeHandler GET /api/v1/nodes/:name
@@ -32,10 +32,10 @@ func GetNodeHandler(c *gin.Context) {
 	key := fmt.Sprintf("/nodes/object/%s", name)
 	err := storage.Get(key, node)
 	if err != nil {
-		c.JSON(http.StatusNotFound, httpData("cannot find resource"))
+		c.JSON(http.StatusNotFound, gin.H{"error": "cannot find resource"})
 		return
 	}
-	c.JSON(http.StatusOK, httpData(utils.JsonMarshal(node)))
+	c.JSON(http.StatusOK, gin.H{"data": utils.JsonMarshal(node)})
 }
 
 // GetAllNodesHandler GET /api/v1/nodes
@@ -44,8 +44,8 @@ func GetAllNodesHandler(c *gin.Context) {
 	var nodes []core.Node
 	err := storage.RangeGet(key, &nodes)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, httpData("cannot read data"))
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "cannot read data"})
 		return
 	}
-	c.JSON(http.StatusOK, httpData(utils.JsonMarshal(nodes)))
+	c.JSON(http.StatusOK, gin.H{"data": utils.JsonMarshal(nodes)})
 }
