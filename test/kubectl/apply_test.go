34971b40fea783da071822cc2293440c70735879
xinyun <starcloud.0416@sjtu.edu.cn>
2024-05-02 18:33:49 +0800

    to finish all: kubectl->get输出直接log,待美化;apply接口还在测试

---
+++ test/kubectl/apply_test.go
@@ -0,0 +1,21 @@
+package test
+
+import (
+	"bytes"
+	"minik8s/pkgs/kubectl/cmd"
+	"testing"
+)
+
+func TestApply(t *testing.T) {
+	/* usable only when api-server is on */
+	actual := new(bytes.Buffer)
+	cmd.RootCommand.SetOut(actual)
+	cmd.RootCommand.SetErr(actual)
+	cmd.RootCommand.SetArgs([]string{"apply", "-f", "../files/createPod.yaml"})
+	cmd.RootCommand.Execute()
+
+	//expected := "This-is-command-a1"
+	//fmt.Print(actual.String())
+	//
+	//assert.Equal(t, actual.String(), expected, "actual is not expected")
+}
