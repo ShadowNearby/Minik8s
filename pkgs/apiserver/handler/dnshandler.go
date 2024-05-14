96ff22a2bf66a10e9859484085a4601f20dd6e47
yan <yanjingsheng@sjtu.edu.cn>
2024-05-14 20:38:23 +0800

    dns config and test

--- pkgs/apiserver/handler/dnshandler.go
+++ pkgs/apiserver/handler/dnshandler.go
@@ -1,9 +1,42 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	core "minik8s/pkgs/apiobject"
+	"minik8s/pkgs/apiserver/storage"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+func DNSKeyPrefix(namespace string, name string) string {
+	return fmt.Sprintf("/dns/object/%s/%s", namespace, name)
+}
+
+func DNSListKeyPrefix(namespace string) string {
+	return fmt.Sprintf("/dns/object/%s", namespace)
+}
 
 // CreateDNSHandler POST /api/v1/namespaces/:namespace/dns
-func CreateDNSHandler(c *gin.Context) {}
+func CreateDNSHandler(c *gin.Context) {
+	var dns core.DNSRecord
+	err := c.Bind(&dns)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "needs dns config type"})
+		return
+	}
+	if dns.MetaData.Namespace == "" {
+		dns.MetaData.Namespace = "default"
+	}
+	key := DNSKeyPrefix(dns.MetaData.Namespace, dns.MetaData.Name)
+	err = storage.Put(key, dns)
+	if err != nil {
+		logrus.Error("error in create dns in storage")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "cannot store data"})
+		return
+	}
+}
 
 // GetDNSHandler GET /api/v1/namespaces/:namespace/dns/:name
 func GetDNSHandler(c *gin.Context) {}
---
+++ utils/hello_server.go
@@ -0,0 +1,51 @@
+package utils
+
+import (
+	"fmt"
+	"os/exec"
+	"strconv"
+
+	"github.com/sirupsen/logrus"
+)
+
+func CreateHelloServer(port uint32, i int) error {
+	createArgs := []string{"run", "-itd", "--name", fmt.Sprintf("hello-server%d", i), "-p",
+		fmt.Sprintf("%d:%d", port, port),
+		"hashicorp/http-echo", "-listen=:" + strconv.Itoa(int(port)), fmt.Sprintf("-text=hello server%d", i)}
+	output, err := exec.Command("docker", createArgs...).CombinedOutput()
+	if err != nil {
+		logrus.Errorf("can not create image error: %s output: %s", err.Error(), output)
+		return err
+	}
+	return nil
+}
+
+func TestHelloServer(addr string, i int) (bool, error) {
+	curlArgs := []string{"-s", addr}
+	output, err := exec.Command("curl", curlArgs...).CombinedOutput()
+	if string(output) != fmt.Sprintf("hello server%d\n", i) {
+		logrus.Errorf("output not match expect: %s, actual: %s", fmt.Sprintf("hello server%d\n", i), string(output))
+		return false, err
+	}
+	if err != nil {
+		logrus.Errorf("can not curl error: %s output: %s", err.Error(), output)
+		return false, err
+	}
+	return true, nil
+}
+
+func DeleteHelloServer(port uint32, i int) error {
+	stopArgs := []string{"stop", fmt.Sprintf("hello-server%d", i)}
+	rmArgs := []string{"rm", fmt.Sprintf("hello-server%d", i)}
+	output, err := exec.Command("docker", stopArgs...).CombinedOutput()
+	if err != nil {
+		logrus.Fatalf("can not create image error: %s output: %s", err.Error(), output)
+		return err
+	}
+	output, err = exec.Command("docker", rmArgs...).CombinedOutput()
+	if err != nil {
+		logrus.Fatalf("can not create image error: %s output: %s", err.Error(), output)
+		return err
+	}
+	return nil
+}
