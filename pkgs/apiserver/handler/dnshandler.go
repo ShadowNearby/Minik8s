9cbbbb676cd7a5d35f02a14a682c9dcccb8c87e1
yan <yanjingsheng@sjtu.edu.cn>
2024-05-17 17:07:43 +0800

    dns update

--- pkgs/apiserver/handler/dnshandler.go
+++ pkgs/apiserver/handler/dnshandler.go
@@ -141,4 +141,88 @@ func DeleteDNSHandler(c *gin.Context) {
 }
 
 // UpdateDNSHandler PUT /api/v1/namespaces/:namespace/dns/:name
-func UpdateDNSHandler(c *gin.Context) {}
+func UpdateDNSHandler(c *gin.Context) {
+	name := c.Param("name")
+	namespace := c.Param("namespace")
+	if len(name) == 0 || len(namespace) == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "needs namespace and name"})
+		return
+	}
+
+	oldRecord := core.DNSRecord{}
+	err := storage.Get(DNSKeyPrefix(namespace, name), &oldRecord)
+	if err != nil {
+		logrus.Error("error in get dns in storage")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "cannot get data"})
+		return
+	}
+
+	oldPath := utils.GenerateDNSPath(oldRecord.Host)
+	if oldPath == "" {
+		logrus.Errorf("generate dns path error")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "cannot generate dns path"})
+		return
+	}
+	err = storage.Del(oldPath)
+	if err != nil {
+		logrus.Errorf("error del dns path: %s", err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "cannot del dns path"})
+		return
+	}
+
+	err = storage.Del(DNSKeyPrefix(namespace, name))
+	if err != nil {
+		logrus.Errorf("error del dns object: %s", err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "cannot del dns object"})
+		return
+	}
+
+	var dnsRecord core.DNSRecord
+	err = c.Bind(&dnsRecord)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "needs dns config type"})
+		return
+	}
+
+	if dnsRecord.MetaData.Namespace == "" {
+		dnsRecord.MetaData.Namespace = "default"
+	}
+	for i, path := range dnsRecord.Paths {
+		if path.IP == "" || path.Port == 0 {
+			ip, port, err := utils.GetServiceAddress(dnsRecord.MetaData.Namespace, path.Service)
+			if err != nil {
+				logrus.Errorf("get service %s:%s addr error", dnsRecord.MetaData.Namespace, path.Service)
+			}
+			dnsRecord.Paths[i].IP = ip
+			dnsRecord.Paths[i].Port = port
+		}
+	}
+	err = storage.Put(DNSKeyPrefix(dnsRecord.MetaData.Namespace, dnsRecord.MetaData.Name), dnsRecord)
+	if err != nil {
+		logrus.Error("error in create dns in storage")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "cannot store data"})
+		return
+	}
+	path := utils.GenerateDNSPath(dnsRecord.Host)
+	if path == "" {
+		logrus.Errorf("generate dns path error")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "cannot generate dns path"})
+		return
+	}
+
+	err = storage.Put(path, core.DNSEntry{Host: config.NginxListenIP})
+	if err != nil {
+		logrus.Errorf("error put DNS path: %s", err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "cannot put dns path"})
+		return
+	}
+
+	err = utils.UpdateNginx()
+	if err != nil {
+		logrus.Errorf("error update nginx %s", err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "cannot update nginx"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": "success"})
+}
